9c63759b1bc0b57d5acb3cb09d930984d4475f56
Jayapal <jayapalinfo@gmail.com>
2022-01-26 20:41:10 +0100

    updated service with getById,create and update

--- src/main/java/com/covestro/productsservice/mapper/ProductsApiModelFormMapper.java
+++ src/main/java/com/covestro/productsservice/mapper/ProductsApiModelFormMapper.java
@@ -1,9 +1,11 @@
 package com.covestro.productsservice.mapper;
 
 import com.covestro.products.api.model.FilteredProduct;
+import com.covestro.products.api.model.ProductReq;
 import com.covestro.productsservice.domain.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 import org.springframework.data.domain.Page;
 
@@ -19,6 +21,11 @@ public interface ProductsApiModelFormMapper {
     @Mappings({
             @Mapping(source = "currentPrice", target = "price"),
     })
+    com.covestro.products.api.model.Product toProductResponse(Product product);
+
+    Product toProduct(ProductReq product);
+
+    void updateProduct(ProductReq form, @MappingTarget Product product);
 
     default FilteredProduct toFilteredProduct(Page<Product> page) {
         return new FilteredProduct()
--- src/main/java/com/covestro/productsservice/service/ProductsService.java
+++ src/main/java/com/covestro/productsservice/service/ProductsService.java
@@ -4,12 +4,18 @@ import com.covestro.products.api.model.FilteredProduct;
 import com.covestro.products.api.model.ProductReq;
 import com.covestro.productsservice.domain.Category;
 import com.covestro.productsservice.domain.Product;
+import com.covestro.productsservice.exception.ProductsRuntimeException;
 import com.covestro.productsservice.mapper.ProductsApiModelFormMapper;
 import com.covestro.productsservice.service.repository.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import com.covestro.products.api.model.Error;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,7 +24,7 @@ public class ProductsService {
     private ProductsRepository productsRepository;
     ProductsApiModelFormMapper productsApiModelFormMapper;
 
-
+    @Autowired
     public ProductsService(ProductsRepository productsRepository, ProductsApiModelFormMapper productsApiModelFormMapper) {
         this.productsRepository = productsRepository;
         this.productsApiModelFormMapper = productsApiModelFormMapper;
@@ -43,24 +49,23 @@ public class ProductsService {
     }
 
     public com.covestro.products.api.model.Product getProductsById(String id) {
-        return null;
+        return productsApiModelFormMapper.toProductResponse(findById(id));
     }
 
     public Product createProduct(ProductReq productReq) {
-        return null;
+        Product product = productsApiModelFormMapper.toProduct(productReq);
+        product.setLastUpdate(LocalDateTime.now());
+        return productsRepository.save(product);
     }
 
-    private Product findById(Integer id) {
-        return null;
+    public Product findById(String id) {
+        return productsRepository.findById(id).orElseThrow(() -> new ProductsRuntimeException(HttpStatus.INTERNAL_SERVER_ERROR, notFound()));
     }
-
 
     public com.covestro.products.api.model.Product updateProduct(String productId, ProductReq productReq) {
-        return null;
-    }
-
-    public Iterable<Product> saveAll(List<Product> users) {
-        return productsRepository.saveAll(users);
+        Product product = findById(productId);
+        productsApiModelFormMapper.updateProduct(productReq, product);
+        return productsApiModelFormMapper.toProductResponse(productsRepository.save(product));
     }
 
     private Sort toSort(String sort) {
@@ -72,6 +77,15 @@ public class ProductsService {
             productSort = Sort.by(sortVals[0]).descending();
         }
         return productSort;
+    }
+
+    private List<Error> notFound() {
+        List<Error> errors = new ArrayList<>();
+        Error error= new Error();
+        error.setCode("404");
+        error.setMessage("Not Found");
+        errors.add(error);
+        return errors;
     }
 
 }
