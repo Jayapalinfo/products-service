06f8a1ce67d36b417b5a73e45805d9ccdd972bb5
Jayapal <jayapalinfo@gmail.com>
2022-01-27 23:04:17 +0100

    load products at startup

--- src/main/java/com/covestro/productsservice/ProductsServiceApplication.java
+++ src/main/java/com/covestro/productsservice/ProductsServiceApplication.java
@@ -1,13 +1,42 @@
 package com.covestro.productsservice;
 
+import com.covestro.productsservice.domain.Product;
+import com.covestro.productsservice.service.ProductsService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @SpringBootApplication
+@Slf4j
 public class ProductsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner loadProducts(ProductsService productsService) {
+		return args -> {
+			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<List<Product>> typeReference = new TypeReference<>() {
+			};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/products.json");
+			try {
+				List<Product> users = mapper.readValue(inputStream, typeReference);
+				productsService.saveAll(users);
+				log.info("Products Saved");
+			} catch (IOException e) {
+				log.error("Error: {}", e.getMessage());
+			}
+		};
 	}
 
 }
